deab2375cf51bb06b0f380b8decd60b76c0f92d2
Shadab Jabbar <shadabjabbar@Shadabs-MacBook-Air.local>
2018-08-19 14:04:54 +0530

    signup payload done

--- src/components/home.js
+++ src/components/home.js
@@ -2,10 +2,59 @@ import React, { Component } from "react";
 import '../App.css';
 
 import {
-    Layout, Header, Navigation,  Grid, Cell, Button
+    Layout, Header, Navigation,  Grid, Cell, Button, Dialog, DialogActions, DialogContent,
+    DialogTitle, RadioGroup, Radio, Textfield
   } from 'react-mdl';
 
+import $ from 'jquery';
+
 class Home extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            ctype:'', c_name:'', cnumber:'', cexpiry:'',ccvv:''
+        };
+        this.handleOpenDialog = this.handleOpenDialog.bind(this);
+        this.handleCloseDialog = this.handleCloseDialog.bind(this);
+    }
+    
+    handleOpenDialog() {
+    this.setState({
+        openDialog: true
+    });
+    }
+
+    handleCloseDialog() {
+    this.setState({
+        openDialog: false
+    });
+    }
+    
+    savepayoption(){
+        let savingData={
+            "cc_number": this.state.cnumber,
+            "cc_expiry": this.state.cexpiry,
+            "cc_cvv": this.state.ccvv,
+            "name": this.state.c_name
+        }
+
+        let payload = JSON.stringify(savingData);
+
+        $.ajax({
+            url:'http://api.esplitzpro.com/v1/cards',
+            data:payload,
+            dataType:'json',
+            contentType:'application/json',
+            method:'POST',
+            success:function(){
+                alert("Successful");
+            },
+            error:function(){
+                alert("Error");
+            }
+        });
+    }
+
     render(){
         return(
             <div>
@@ -14,7 +63,7 @@ class Home extends Component{
                         <Header transparent title="Welcome: John Doe!" style={{color: '#c00', 
                         backgroundColor:'whitesmoke', position:'fixed', top:'0'}}>
                             <Navigation>
-                                <a style={{color: '#c00'}}><Button>Manage Payment Options</Button></a>
+                                <a style={{color: '#c00'}}><Button onClick={this.handleOpenDialog}>Manage Payment Options</Button></a>
                                 <a style={{color: '#c00'}}><Button>Account Settings</Button></a>
                                 <a style={{color: '#c00'}}><Button>Help</Button></a>
                                 <a style={{color: '#c00'}}><Button>Logout</Button></a>
@@ -25,7 +74,29 @@ class Home extends Component{
                 <div className="content">
                     <Grid style={{padding:'0px'}}>
                         <Cell col={12} style={{margin:'0px', color:'black'}}>
-                            <h1>Main</h1>
+                            <Dialog open={this.state.openDialog} style={{width:'40%'}}>
+                                <DialogTitle style={{textAlign:'center'}}>Your Payment Options</DialogTitle>
+                                <p style={{borderBottom:'0.5px solid #c00', margin:'10px'}}></p>
+                                <DialogContent>
+                                    <div style={{textAlign:'center'}}>
+                                        <RadioGroup name="paytype" value="cc">
+                                            <Radio value="cc" ripple className="single-radio">Credit Card</Radio>
+                                            <Radio value="dc" ripple className="single-radio">Debit Card</Radio>
+                                            <Radio value="ba" ripple className="single-radio" disabled>Bank Account</Radio>
+                                        </RadioGroup>
+                                    </div>
+                                    <div style={{textAlign:'center', marginTop:'20px'}}>
+                                        <Textfield label="Name" floatingLabel style={{width:'80%'}} onChange={(e) => this.setState({c_name:e.target.value})}/>
+                                        <Textfield label="Card Number" floatingLabel style={{width:'80%'}} onChange={(e) => this.setState({cnumber:e.target.value})}/>
+                                        <Textfield label="Expiry Date" floatingLabel style={{width:'80%'}} onChange={(e) => this.setState({cexpiry:e.target.value})}/>
+                                        <Textfield label="Card CVV" floatingLabel style={{width:'80%'}} onChange={(e) => this.setState({ccvv:e.target.value})}/>
+                                    </div>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button type='button' onClick={this.savepayoption.bind(this)}>Verify and Save</Button>
+                                    <Button type='button' onClick={this.handleCloseDialog}>Cancel</Button>
+                                </DialogActions>
+                            </Dialog>
                         </Cell>
                     </Grid>
                 </div>
--- src/components/signup.js
+++ src/components/signup.js
@@ -6,11 +6,48 @@ import {
   CardActions, Button, Textfield, RadioGroup, Radio
 } from 'react-mdl';
 
+import $ from 'jquery';
+
 class Signup extends Component {
+  constructor(){
+    super();
+    this.state={
+      username:'', email:'', phone:'', password:''
+    }
+  }
+
+  comparePass(e){
+
+  }
+
+  signup(e){
+    let savingData={
+      "name":this.state.username,
+      "email":this.state.email,
+      "phone":this.state.phone,
+      "password":this.state.password
+    }
+
+    let payload = JSON.stringify(savingData);
+    $.ajax({
+      url:'http://api.esplitzpro.com/v1/users',
+      data:payload,
+      dataType:'json',
+      method:'POST',
+      contentType:'application/json',
+      success:function(data, response){
+        alert(data.message);
+      },
+      error:function(){
+        alert("Error");
+      }
+    });
+  }
+
   render() {
     return (
       <div>
-        <div style={{height: '600px', position: 'relative'}}>
+        <div style={{height: '700px', position: 'relative'}}>
             <Layout>
                 <Header transparent title="eSplitz" style={{color: '#c00', backgroundColor:'#fff', position:'fixed', top:'0'}}>
                     <Navigation>
@@ -28,26 +65,26 @@ class Signup extends Component {
                         <img src={require('../assests/banner.jpg')} alt="bnimg" style={{width:'100vw'}}/>
                     </Cell>
                     <Cell col={4}>
-                      <Card shadow={0} style={{width: '80%', margin: 'auto', height:'54%'}}>
+                      <Card shadow={0} style={{width: '80%', margin: 'auto', height:'64%'}}>
                         <CardTitle style={{height: '100px', margin:'0px', justifyContent:'center'}}>
                           Welcome to eSplitz
                         </CardTitle>
                         <p style={{textAlign:'center'}}>Already a member ? <a href="/#/">Login</a></p>
-                        <p style={{textAlign:'center', margin:'20px'}}>
-                            <RadioGroup name="demo" value="opt1">
-                                <Radio value="opt1" ripple>Personal</Radio>
-                                <Radio value="opt2">Business</Radio>
+                        <div style={{textAlign:'center'}}>
+                            <RadioGroup name="acctype" value="pers">
+                                <Radio value="pers" ripple className="single-radio">Personal</Radio>
+                                <Radio value="buss" ripple>Business</Radio>
                             </RadioGroup>
-
-                        </p>
+                        </div>
                         <CardText style={{textAlign:'center'}}>
-                            <Textfield label="Username" floatingLabel />
-                            <Textfield label="Email" floatingLabel />
-                            <Textfield label="Password" type="password" floatingLabel />
-                            <Textfield label="Confirm Password" type="password" floatingLabel />
+                            <Textfield label="Username*" floatingLabel onChange={(e) => this.setState({username:e.target.value})} id="username"/>
+                            <Textfield label="Email*" floatingLabel onChange={(e) => this.setState({email:e.target.value})} id="email"/>
+                            <Textfield label="Phone*" floatingLabel pattern="-?[0-9]*(\.[0-9]+)?" error="Invalid Phone Number!" onChange={(e) => this.setState({phone:e.target.value})} id="phone"/>
+                            <Textfield label="Password*" type="password" floatingLabel onChange={(e) => this.setState({password:e.target.value})} id="password"/>
+                            <Textfield label="Confirm Password*" type="password" floatingLabel onChange={this.comparePass.bind(this)} />
                         </CardText>
                         <CardActions style={{textAlign:'center'}}>
-                            <Button raised style={{width:'85%', margin:'auto 10px', backgroundColor:'#c00', color:'#fff'}}>SIGN UP</Button>
+                            <Button raised style={{width:'85%', margin:'auto 10px', backgroundColor:'#c00', color:'#fff'}} onClick={this.signup.bind(this)}>SIGN UP</Button>
                         </CardActions>
                       </Card>
                     </Cell>
